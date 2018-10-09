7415714e0e866c05ab78ad7bb93f0db90f1be9a8
Jiamin Lim <jiaminlim93@gmail.com>
2018-10-09 19:34:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+/*function getFirstSelector(selector){
+  return   document.querySelector(selector)[0]
+}
+
+function nestedTarget(){
+  
+}
+*/
+
+function increaseRankBy(n){
+  const lis = document.getElementById('all').querySelectorAll('ul.ranked-list li')
+  
+  for (let i=0; i<lis.length; i++){
+    lis[i].innerHTML=parseInt(lis[i].innerHTML)+n
+  }
+}
 
