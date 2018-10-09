96fbfbc182f2dbaa1e64adf9ac854eadf30565a3
Jiamin Lim <jiaminlim93@gmail.com>
2018-10-09 19:53:25 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector)
+}
+
+function nestedTarget(){
+  return document.querySelector('#nested div div div div.target')
+}
+
+
+function increaseRankBy(n){
+  const lis = document.getElementById('app').querySelectorAll('ul.ranked-list li');
+  
+  for (let i=0; i<lis.length; i++){
+    lis[i].innerHTML=parseInt(lis[i].innerHTML)+n
+  }
+}
+
+function deepestChild(){
+  let node = document.getElementById('grand-node')
+  let nextNode = node.children[0]
+  
+  while(nextNode){
+    node=nextNode
+    nextNode= node.children[0]
+  }
+  
+  return node
+}
 
